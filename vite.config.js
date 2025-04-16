5fa0bf55943a2976c63be638d93ce09199a43c03
mama <muses@act.com.et>
2025-04-16 14:11:22 +0300

    add some changes for guthub workflow

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/portfolio/",
   plugins: [react()],
-  build: { chunkSizeWarningLimit: 1600 },
+  build: { chunkSizeWarningLimit: 1600, outDir: "dist" },
 });
