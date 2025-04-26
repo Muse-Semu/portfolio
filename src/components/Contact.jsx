e767dee794d5225501f0faa09728c1c2106e41c6
mama <muses@act.com.et>
2025-04-26 22:30:23 +0300

    Update placeholders at contant form.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -150,7 +150,7 @@ const Contact = () => {
               name="name"
               value={form.name}
               onChange={handleChange}
-              placeholder="What's your good name?"
+              placeholder="What's your name?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
               required
             />
@@ -162,7 +162,7 @@ const Contact = () => {
               name="email"
               value={form.email}
               onChange={handleChange}
-              placeholder="What's your web address?"
+              placeholder="What's your email address?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
               required
             />
