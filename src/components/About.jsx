a21e3bd7a0b4ced4600d3895cf1093f352d01307
mama <smuse1756@gmail.com>
2025-06-11 22:04:56 +0300

    Edit some contents and add vercel json

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,7 +47,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a Senior Full Stack Developer with a strong backend focus, I
+        I'm a Full Stack Developer with a strong backend focus, I
         specialize in architecting scalable web applications, excelling in
         Spring Boot for high-performance backend systems. I design
         microservices, optimize PostgreSQL databases, and integrate seamlessly
